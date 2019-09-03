63568567a3fb347138fe2d2dbb5f5f1b02710634
Italo Bianchini <italo@in8.com.br>
2019-09-03 14:36:41 -0300

    Initial Commit

---
+++ CleanArchitecture+Coordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanArchitecture+Coordinator
+//
+//  Created by Ítalo Bianchini on 03/09/19.
+//  Copyright © 2019 BianchiniItalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
